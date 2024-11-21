1b97d9c68e4ca2443bd67ac674df0fc8a17544ee
sweetipal <sweetipal752@gmail.com>
2024-11-21 23:55:35 +0530

    generate a component

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Import CommonModule for Angular directives
+import { Todo } from '../../Todo';
 
 @Component({
   selector: 'app-todos',
-  imports: [],
+//  standalone: true,  // Mark this component as standalone (if Angular 14+)
+  imports: [CommonModule],  // Add CommonModule to imports for *ngFor and *ngIf
   templateUrl: './todos.component.html',
-  styleUrl: './todos.component.css'
+  styleUrls: ['./todos.component.css']
 })
 export class TodosComponent {
-
+  todos: Todo[] = [
+    {
+      sno: 1,
+      title: "This is title",
+      desc: "This is desc",
+      active: true
+    },
+    {
+      sno: 2,
+      title: "This is title",
+      desc: "This is desc",
+      active: true
+    },
+    {
+      sno: 3,
+      title: "This is title",
+      desc: "This is desc",
+      active: true
+    }
+  ];
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TodosComponent } from './MyComponents/todos/todos.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TodosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'sp-todo-list';
-  constructor(){
-    setTimeout(() =>{
-      this.title = "Changed Title";
-    },2000)
-  }
+  // constructor(){
+  //   setTimeout(() =>{
+  //     this.title = "Changed Title";
+  //   },2000)
+  // }
   
 }
